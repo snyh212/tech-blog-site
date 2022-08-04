9e4089f20902121ff24a040aef976e89e83bfbf1
Shmuel Hoffman <snyh212@gmail.com>
2022-08-04 01:19:42 -0400

    added password hasher

--- models/User.js
+++ models/User.js
@@ -6,20 +6,50 @@ const sequelize = require('../config/connection');
 const bcrypt = require('bcrypt');
 
 //create User model
-class User extends Model {}
+class User extends Model {
+    checkPassword(loginPw) {
+        return bcrypt.compareSync(loginPw, this.password);
+    }
+}
 // define fields/columns id, username, password
 User.init(
-    {  
-        id: {},
+        {  
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        username: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+            len: [6, 20]
+            }
+        }
     },
     {
-      sequelize,
-      timestamps: false,
-      freezeTableName: true,
-      underscored: true,
-      modelName: 'user'
+        //password hash
+        hooks: {
+            async beforeCreate(hashPass) {
+                hashPass.password = await bcrypt.hash(hashPass.password, 10);
+                return hashPass;
+            },
+            async beforeUpdate(hashPass) {
+                hashPass.password = await bcrypt.hash(hashPass.password, 10);
+                return hashPass;
+            }
+        },
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'user'
     }
 );
-//password hash
 //modlue export
 module.exports = User;
