5e29d9651bda940170ddbdd07f34f6c2a7cde074
Shmuel Hoffman <snyh212@gmail.com>
2022-08-04 01:25:55 -0400

    added comment model

--- models/Comment.js
+++ models/Comment.js
@@ -7,15 +7,43 @@ class Comment extends Model {}
 
 // define fields/columns id, userId, postId, comment
 Comment.init(
-    {  
-        id: {},
-    },
-    {
-      sequelize,
-      freezeTableName: true,
-      underscored: true,
-      modelName: 'comment'
-    }
+  {
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        allowNull: false,
+        autoIncrement: true
+      },
+      user_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'user',
+          key: 'id'
+        }
+      },
+      post_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'post',
+          key: 'id'
+        }
+      },
+      comment_text: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+            len: [1]
+        }
+      }
+  },
+  {
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'comment'
+  }
 );
 
 //modlue export
