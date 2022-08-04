181250038fa0267a5ebed64926493bc014db272b
Shmuel Hoffman <snyh212@gmail.com>
2022-08-04 01:27:44 -0400

    added model associations in index.js

--- models/index.js
+++ models/index.js
@@ -5,10 +5,34 @@ const Comment = require('./Comment');
 
 //create associations with foreign key associations
 //user many posts
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+});
+
 //user many comment
+User.hasMany(Comment, {
+    foreignKey: 'user_id'
+});
+
 //post belong to user
+Post.belongsTo(User, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+});
+
 //post many comment
+Post.hasMany(Comment, {
+    foreignKey: 'post_id'
+});
+
 //comment belong to user
+Comment.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
 //comment belong to post
+Comment.belongsTo(Post, {
+    foreignKey: 'post_id'
+});
 
 module.exports = {User, Post, Comment};
