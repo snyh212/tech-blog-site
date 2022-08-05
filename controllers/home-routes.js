e1a306c8940e4846505e1df42ce80277230b8e0d
Shmuel Hoffman <snyh212@gmail.com>
2022-08-05 01:55:27 -0400

    added homeroutes

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,8 +1,41 @@
 //require routes
 const router = require('express').Router();
 const { Post, User, Comment } = require('../models');
+
 //GET all posts
+router.get('/', (req, res) => {
+    Post.findAll({
+        attributes: [
+            'id',
+            'title',
+            'post_content',
+            'date_created'
+        ],
+        include: [
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'date_created'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            }
+        ]
+    })
+        .then(dbPostData => {
+            const posts = dbPostData.map(post => post.get({ plain: true }));
+            res.render('homepage', { posts, loggedIn: req.session.loggedIn });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
 //GET 1 Post
 //GET login
 //GET logout
 //GET signup
+
+
+module.exports = router;
