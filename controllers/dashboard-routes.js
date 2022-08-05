54666f3563a1547364abdf13cc43c1c18ef3ee3d
Shmuel Hoffman <snyh212@gmail.com>
2022-08-05 01:58:59 -0400

    added dashboard routes

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -2,4 +2,43 @@
 const router = require('express').Router();
 const { Post, Comment, User } = require('../models/');
 const withAuth = require('../utils/auth');
+
 //GET all posts
+router.get('/', withAuth, (req, res) => {
+    Post.findAll({
+        where: {
+            user_id: req.session.user_id
+        },
+        attributes: [
+            'id',
+            'title',
+            'post_content',
+            'date_created'
+        ],
+        include: [
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'date_created'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+    .then(dbPostData => {
+        const post = dbPostData.map(post => post.get({ plain: true }));
+        res.render('dashboard', { post, loggedIn: true });
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+  });
+
+
+module.exports = router;
