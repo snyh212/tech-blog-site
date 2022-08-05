3350554c193ef98767ea4d4542da809cbf77e1b5
Shmuel Hoffman <snyh212@gmail.com>
2022-08-05 02:05:38 -0400

    added post get routes

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -4,7 +4,77 @@ const { Post, Comment, User } = require('../../models/');
 const withAuth = require('../../utils/auth');
 
 //GET all posts
+router.get('/', (req, res) => {
+    Post.findAll({
+      attributes: [
+        'id',
+        'title',
+        'post_content',
+        'date_created'
+      ],
+      order: [['date_created', 'DESC']],
+      include: [
+        {
+          model: Comment,
+          attributes: ['id', 'comment_text', 'post_id', 'user_id', 'date_created'],
+          include: {
+            model: User,
+            attributes: ['username']
+          }
+        },
+        {
+          model: User,
+          attributes: ['username']
+        }
+      ]
+    })
+      .then(dbPostData => res.json(dbPostData))
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
+
 //GET 1 post
+router.get('/:id', (req, res) => {
+    Post.findOne({
+      where: {
+        id: req.params.id
+      },
+      attributes: [
+        'id',
+        'title',
+        'post_content',
+        'date_created'
+      ],
+      include: [
+        {
+          model: Comment,
+          attributes: ['id', 'comment_text', 'post_id', 'user_id', 'date_created'],
+          include: {
+            model: User,
+            attributes: ['username']
+          }
+        },
+        {
+          model: User,
+          attributes: ['username']
+        }
+      ]
+    })
+      .then(dbPostData => {
+        if (!dbPostData) {
+          res.status(404).json({ message: 'No post with this id' });
+          return;
+        }
+        res.json(dbPostData);
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
+
 //POST post
 //PUT (update post)
 //DELETE 
