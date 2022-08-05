c745b181dfd6801fa2360890b4c515dabf0d2952
Shmuel Hoffman <snyh212@gmail.com>
2022-08-05 01:57:43 -0400

    added rest of home routes

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -33,9 +33,61 @@ router.get('/', (req, res) => {
 });
 
 //GET 1 Post
+router.get('/post/:id', (req, res) => {
+    Post.findOne({
+        where: {
+            id: req.params.id
+        },
+        attributes: [
+            'id',
+            'title',
+            'post_content',
+            'date_created'
+        ],
+        include: [
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'date_created'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+        .then(dbPostData => {
+            if (!dbPostData) {
+                res.status(404).json({ message: 'No post found with this id' });
+                return;
+            }
+
+            const post = dbPostData.get({ plain: true });
+
+            res.render('single-post', { post, loggedIn: req.session.loggedIn });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
 //GET login
-//GET logout
+router.get('/login', (req, res) => {
+    if (req.session.loggedIn) {
+        res.redirect('/dashboard');
+        return;
+    }
+    res.render('login');
+});
+
 //GET signup
+router.get('/signup', (req, res) => {
+    res.render('signup');
+});
 
 
 module.exports = router;
