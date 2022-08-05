ebfed07b7fd92c68c517be81c12df3c46cd91be3
Shmuel Hoffman <snyh212@gmail.com>
2022-08-05 02:00:59 -0400

    added user get routes

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,8 +1,58 @@
 //require routes
 const router = require('express').Router();
 const { User, Post, Comment } = require('../../models');
+
 //GET all users
+router.get('/', (req, res) => {
+    User.findAll({
+        attributes: { exclude: ['password'] }
+    })
+        .then(dbUserData => res.json(dbUserData))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
 //GET 1 user
+router.get('/:id', (req, res) => {
+    User.findOne({
+        attributes: { exclude: ['password'] },
+        where: {
+            id: req.params.id
+        },
+        include: [
+            {
+                model: Post,
+                attributes: ['id', 'title', 'post_content', 'date_created']
+            },
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'date_created'],
+                include: {
+                    model: Post,
+                    attributes: ['title']
+                }
+            }
+        ]
+    })
+        .then(dbUserData => {
+            if (!dbUserData) {
+                // if user with nonexistent id is searched, return error 404 with message. 
+                res.status(404).json({ message: 'No user found with this id' });
+                return;
+            }
+            res.json(dbUserData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
 //POST create user
 //POST login
 //POST logout
+
+
+module.exports = router;
